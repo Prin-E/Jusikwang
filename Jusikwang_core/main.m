3704883995d6da88adaa83019e4d37b677e48f5b
이현우 <leehyeonwoo@HyeonWoo-MacBook.local>
2011-12-11 15:43:08 +0900

    Initial Commit

---
+++ Jusikwang_core/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Jusikwang_core
+//
+//  Created by 이 현우 on 11. 12. 11..
+//  Copyright (c) 2011 서울시립대학교. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
