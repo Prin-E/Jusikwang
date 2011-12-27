660318cc89277238756a82e20547b6af85828b6f
Prin_E <ddderer@gmail.com>
2011-12-28 06:23:42 +0900

    - JusikRange의 문제점을 수정하였습니다. - JusikStockMarket 에서 이제 미국주가,종합주가,환율 변동을 계산할 수 있습니다. - JusikStockFunction 에서 변수 F까지의 계산 구현이 완료되었습니다. 나머지 부분의 구현은 이벤트 처리 부분을 정리한 후에 구현될 것입니다. - 이제 JusikRecord 에서 5/20/34일선을 구할 때 최근까지의 n일을 지정해서 구할 수 있습니다(성능문제로 수정된 부분입니다). - 메모리 누수 부분 수정.

---
+++ Jusikwang/JusikLogoViewController.h
@@ -0,0 +1,13 @@
+//
+//  JusikLogoViewController.h
+//  Jusikwang
+//
+//  Created by 이 현우 on 11. 12. 28..
+//  Copyright (c) 2011 서울시립대학교. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JusikLogoViewController : UIViewController
+
+@end
